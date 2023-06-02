f2ee9030b6f65b764bc5924efa1ccef0b449b3d2
Eduardo Duque <edunduque@gmail.com>
2023-06-02 14:02:04 -0700

    finished product

--- rangers_rock_paper_scrissors.py
+++ rangers_rock_paper_scrissors.py
@@ -1,31 +1,75 @@
 import random
 
-rps_list = ['rock', 'paper', 'scissors']
-computer_choice = random.choice(rps_list)
 
 
-while True:
+def player1():
+    rps_list = ['Rock', 'Paper', 'Scissors']
+    Player1_Wins = 0
+    CPU_Wins = 0
     r = 'Rock'
     p = 'Paper'
-    s = 'Scissors'    
-    print("--Rock, Paper, Scissors--")
-    prompt = input("Would you like to play a game of rock, paper, scissors?\n Yes('Y') No('N') or Quit('Q')")
+    s = 'Scissors'
+    computer_choice = random.choice(rps_list)
 
+
+    print("--Rock, Paper, Scissors--")
+    prompt = input("Would you like to play a game of rock, paper, scissors?\n Yes('Y') No('N')")
     if prompt.lower() == 'n':
         print("\nWow, that scared to lose huh? Come back when you want to compete!")
-        break
-    #elif prompt.lower() != 'n' or prompt.lower() != 'y' or prompt.lower() != 'q':
-        #print("invalid entry, please select from the choices given")
-    elif prompt.lower() == 'q':
-        print("Come back when you want to play again")
-        #print(wins/losses)
-        break
     elif prompt.lower() == 'y':
-        print("\nLet's do it!")
-        print("What's it gonna be?")
-        choice = input("Rock('r') Paper('p') Scissors('s'): ")
-        if choice.lower() == r or p or s:
-            print(f"You Chose {choice}")
+        while True:
+            print("\nLet's do it!")
+            print("What's it gonna be?")
+            choice = input("Rock('r') Paper('p') Scissors('s'): ")
+            if choice == 'r':
+                computer_choice = random.choice(rps_list)
+                print(f"You Chose {r}")
+                print(f"I Chose {computer_choice}")
+                if computer_choice == 'Rock':
+                    print('\nIt\'s a draw!')
+                elif computer_choice == 'Paper':
+                    print('\nPaper beats rock, I win!!')
+                    CPU_Wins += 1
+                    print('Player 1 Score: ', Player1_Wins, 'CPU Score: ', CPU_Wins)
+                elif computer_choice == 'Scissors':
+                    print('\nWow, defeated...')
+                    Player1_Wins += 1
+                    print('Player 1 Score: ', Player1_Wins, 'CPU Score: ', CPU_Wins)
+            if choice == 'p':
+                computer_choice = random.choice(rps_list)
+                print(f"You Chose {p}")
+                print(f"I Chose {computer_choice}")
+                if computer_choice == 'Rock':
+                    print('\nWow, defeated...')
+                    Player1_Wins += 1
+                    print('Player 1 Score: ', Player1_Wins, 'CPU Score: ', CPU_Wins)
+                elif computer_choice == 'Paper':
+                    print('\nIt\'s a draw!')
+                elif computer_choice == 'Scissors':
+                    print('\nScissors beats paper, I win!!')
+                    CPU_Wins += 1
+                    print('Player 1 Score: ', Player1_Wins, 'CPU Score: ', CPU_Wins)
+            if choice == 's':
+                computer_choice = random.choice(rps_list)
+                print(f"You Chose {s}")
+                print(f"I Chose {computer_choice}")
+                if computer_choice == 'Rock':
+                    print('\nRock beats scissors, I win!!')
+                    CPU_Wins += 1
+                    print('Player 1 Score: ', Player1_Wins, 'CPU Score: ', CPU_Wins)
+                elif computer_choice == 'Paper':
+                    print('\nWow, defeated...')
+                    Player1_Wins += 1
+                    print('Player 1 Score: ', Player1_Wins, 'CPU Score: ', CPU_Wins)
+                elif computer_choice == 'Scissors':
+                    print('\nIt\'s a draw!')
+            repeat = input('\nWant to play again? Yes("y") No("n"): ')
+            if repeat.lower() != 'y':
+                print('\nThank you for playing!')
+                print('Here are the finishing scores:')
+                print('Player 1: ', Player1_Wins, 'CPU: ', CPU_Wins)
+                break
+    else:
+        print('\nInvalid entry, restart and select from the options given.')
 
-
-
+player1()
